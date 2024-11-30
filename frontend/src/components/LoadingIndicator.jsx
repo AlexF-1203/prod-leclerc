cfe7bdb229f3b47bb0760c5798773b72ad627efa
Alexis Ferrere <alexferrere@outlook.fr>
2024-11-30 22:44:09 +0100

    First Commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+import "../styles/LoadingIndicator.css";
+
+const LoadingIndicator = () => {
+    return (
+        <div className="loader-icontainer">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default LoadingIndicator;
